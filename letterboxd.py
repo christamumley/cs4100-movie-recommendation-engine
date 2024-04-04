763d23ae997a7293d581ba621e07643395cb51e9
sm1se <rosemary.s@northeastern.edu>
2024-04-04 00:42:05 -0400

    updated letterboxd.py to include a mapping of movies to genres and themes

--- letterboxd.py
+++ letterboxd.py
@@ -14,8 +14,9 @@ def get_watched(username):
 
 def get_movie_genres(movies) :
     # get html for each movie
-    genres = []
-    themes = []
+    genres_total = []
+    themes_total = []
+    my_dict = {}
     for movie in movies:
 
         formatted_movie = movie.lower().replace(' ', '-')
@@ -25,23 +26,43 @@ def get_movie_genres(movies) :
         
         # get genres
         genre_themes_parent = soup.find(id='tab-genres')
-        div_elements = genre_themes_parent.find_all('div')
+        try:
+            div_elements = genre_themes_parent.find_all('div')
+        except AttributeError:
+            continue
+
         # Get the second div element
         
         genre_tab = genre_themes_parent.find('div', class_= 'text-sluglist capitalize')
         genre_tags = genre_tab.find_all(class_='text-slug')
         genres = [tag.text.strip() for tag in genre_tags]
+        genres_total.append(genres)
 
         if len(div_elements) > 1:
             theme_tab = genre_tab.find_next_sibling('div', class_= 'text-sluglist capitalize')
             theme_tags = theme_tab.find_all(class_='text-slug')
             themes = [tag.text.strip() for tag in theme_tags]
+            themes_total.append(themes)
+            my_dict[movie] = [genres, themes]
+        else:
+            my_dict[movie] = [genres]
 
-
-    return genres, themes
-
+    return genres_total, themes_total, my_dict
+#Some ideas on what we can do with this data, we can find the most common genres, or themes present and recommend movies based on that. 
+#(We can see which themes are most often seens with certain genres, and recommend movies that have both those themes and genres)
+#maybe map the genres to the themes?
 movies_test= get_watched('jay')
 print(movies_test)
 
-genres_test, themes_test = get_movie_genres(['ferrari-2023'])
-print(genres_test, themes_test)
+genres_test, themes_test, dict = get_movie_genres(movies_test)
+print(dict)
+
+'''
+['We Live in Time', 'Irish Wish', 'Crossing', 'Madame Web', 'Dune: Part Two', 'My Wonderful Stranger', 'Love Lies Bleeding', 'Doctor Who: The Church on Ruby Road', 'Doctor Who: The Giggle', 'Doctor Who: Wild Blue Yonder', 'Wonka', 'Doctor Who: The Star Beast', 'Merchant Ivory', 'The Iron Claw', 'The Hunger Games: The Ballad of Songbirds & Snakes', 'Thanksgiving', "Five Nights at Freddy's", 'Chicken Run: Dawn of the Nugget', 'The Exorcist: Believer', 'Saw X', 'No One Will Save You', 'A Haunting in Venice', 'American Fiction', 'Hit Man', 'Priscilla', 'The Killer', 'Maestro', 'NYAD', 'Hoard', 'Poor Things', 'The Royal Hotel', 'The Wonderful Story of Henry Sugar', 'All of Us Strangers', 'Saltburn', 'The Holdovers', 'Ferrari', 'The Bikeriders', 'El Conde', 'Doctor Jekyll', 'Retribution', 'Gran Turismo', 'Teenage Mutant Ninja Turtles: Mutant Mayhem', 'The Boy and the Heron', 'Oppenheimer', 'Barbie', 'Insidious: The Red Door', 'Paradise', 'Mission: Impossible – Dead Reckoning Part One', 'Black Mirror: Demon 79', 'Black Mirror: Mazey Day', 'Black Mirror: Beyond the Sea', 'Black Mirror: Loch Henry', 'Black Mirror: Joan Is Awful', 'They Cloned Tyrone', 'The Flash', 'Meg 2: The Trench', 'Spider-Man: Across the Spider-Verse', 'La Chimera', 'In Our Day', 'Cobweb', 'The Taste of Things', 'Asteroid City', 'Kidnapped', 'Kubi', 'Fallen Leaves', 'The Boogeyman', 'Club Zero', 'Anatomy of a Fall', 'Killers of the Flower Moon', 'May December', 'The Zone of Interest', 'How to Have Sex']
+[['Drama', 'Romance'], ['Romance', 'Fantasy', 'Comedy'], ['Fantasy', 'Action'], ['Drama'], ['Thriller', 'Crime', 'Action'], ['Comedy', 'Fantasy', 'Family'], ['Documentary'], ['Action', 'Adventure'], ['Drama'], ['Thriller', 'Horror'], ['Science Fiction', 'Thriller', 'Horror'], ['Crime', 'Mystery', 'Thriller'], ['Comedy', 'Drama'], ['Action'], ['Romance', 'Drama'], ['Crime', 'Drama', 'Action', 'Thriller'], ['Animation'], ['Drama', 'History'], ['Drama'], ['Thriller'], ['Drama', 'Thriller'], ['Fantasy', 'Comedy', 'Adventure'], ['Romance', 'Drama', 'Fantasy'], ['Comedy', 'Drama', 'Thriller'], ['Comedy', 'Drama'], ['Drama'], ['Crime', 'Drama'], ['Horror', 'Fantasy', 'Comedy'], ['Horror'], ['Horror', 'Thriller', 'Mystery'], ['Adventure', 'Action', 'Drama'], ['Animation', 'Adventure', 'Fantasy'], ['Drama'], ['Comedy', 'Adventure'], ['Adventure', 'Romance'], ['Comedy', 'Science Fiction', 'Mystery'], ['Science Fiction', 'Action', 'Adventure'], ['Comedy', 'Adventure', 'Drama', 'Fantasy'], ['Drama'], ['Music', 'Comedy', 'Romance'], ['Romance', 'Drama'], ['Comedy', 'Drama'], ['Adventure', 'Family', 'Action', 'Drama'], ['Action', 'Drama', 'History'], ['Drama'], ['Mystery', 'Horror', 'Thriller'], ['Comedy', 'Thriller', 'Drama'], ['Mystery', 'Crime', 'Thriller'], ['History', 'Crime', 'Drama'], ['Drama', 'Comedy'], ['Drama', 'History', 'War'], ['Drama']] 
+[['Relationship comedy', 'Laugh-out-loud relationship entanglements', 'Charming romances and delightful chemistry', 'Quirky and endearing relationships', 'Captivating relationships and charming romance', 'Touching and sentimental family stories', 'Show All…'], ['Epic heroes', 'Superheroes in action-packed battles with villains', 'Show All…'], ['Song and dance', 'Humanity and the world around us', 'Crude humor and satire', 'Dazzling vocal performances and musicals', 'Holiday joy and heartwarming Christmas', 'Fairy-tale fantasy and enchanted magic', 'Catchy songs and hilarious musical comedy', "Kids' animated fun and adventure", 'Show All…'], ['Horror, the undead and monster classics', 'Graphic violence and brutal revenge', 'Gory,  gruesome,  and slasher horror', 'Twisted dark psychological thriller', 'Show All…'], ['Monsters, aliens, sci-fi and the apocalypse', 'Horror, the undead and monster classics', 'Sci-fi horror,  creatures,  and aliens', 'Terrifying,  haunted,  and supernatural horror', 'Twisted dark psychological thriller', 'Thought-provoking sci-fi action and future technology', 'Action-packed space and alien sagas', 'Show All…'], ['Thrillers and murder mysteries', 'Intriguing and suspenseful murder mysteries', 'Suspenseful crime thrillers', 'Terrifying,  haunted,  and supernatural horror', 'Noir and dark crime dramas', 'Twisted dark psychological thriller', 'Show All…'], ['Crude humor and satire', 'Humanity and the world around us', 'Moving relationship stories', 'Relationship comedy', 'Quirky and endearing relationships', 'Amusing jokes and witty satire', 'Funny jokes and crude humor', 'Gags,  jokes,  and slapstick humor', 'Teen school antics and laughter', 'Show All…'], ['Moving relationship stories', 'Passion and romance', 'Emotional teen coming-of-age stories', 'Enduring stories of family and marital drama', 'Emotional life of renowned artists', 'Powerful stories of heartbreak and suffering', 'Show All…'], ['Crime, drugs and gangsters', 'High speed and special ops', 'Moving relationship stories', 'Epic heroes', 'Violent action,  guns,  and crime', 'Intense combat and martial arts', 'Gritty crime and ruthless gangsters', 'Explosive and action-packed heroes vs. villains', 'Adrenaline-fueled action and fast cars', 'Show All…'], ['Humanity and the world around us', 'Inspiring sports underdog stories', 'Show All…'], ['Horror, the undead and monster classics', 'Terrifying,  haunted,  and supernatural horror', 'Show All…'], ['Humanity and the world around us', 'Emotional and captivating fantasy storytelling', 'Show All…'], ['Moving relationship stories', 'Humanity and the world around us', 'Powerful stories of heartbreak and suffering', 'Emotional LGBTQ relationships', 'Touching and sentimental family stories', 'Emotional and touching family dramas', 'Emotional teen coming-of-age stories', 'Show All…'], ['Intense violence and sexual transgression', 'Twisted dark psychological thriller', 'Challenging or sexual themes & twists', 'Erotic relationships and desire', 'Dreamlike,  quirky,  and surreal storytelling', 'Graphic violence and brutal revenge', 'Show All…'], ['Moving relationship stories', 'Underdogs and coming of age', 'Student coming-of-age challenges', 'Emotional and touching family dramas', 'Touching and sentimental family stories', 'Quirky and endearing relationships', 'Powerful stories of heartbreak and suffering', 'Show All…'], ['Crude humor and satire', 'Bloody vampire horror', 'Creepy,  chilling,  and terrifying horror', 'Gothic and eerie haunting horror', 'Show All…'], ['Thrillers and murder mysteries', 'Intriguing and suspenseful murder mysteries', 'Suspenseful crime thrillers', 'Terrifying,  haunted,  and supernatural horror', 'Gothic and eerie haunting horror', 'Gory,  gruesome,  and slasher horror', 'Show All…'], ['Moving relationship stories', 'Adrenaline-fueled action and fast cars', 'Inspiring sports underdog stories', 'Underdog fighting and boxing stories', 'Show All…'], ['Humanity and the world around us', 'Surreal and thought-provoking visions of life and death', 'Emotional and captivating fantasy storytelling', 'Dreamlike,  quirky,  and surreal storytelling', 'Fairy-tale fantasy and enchanted magic', 'Captivating relationships and charming romance', 'Show All…'], ['Humanity and the world around us', 'Crude humor and satire', 'Moving relationship stories', 'Emotional and captivating fantasy storytelling', 'Surreal and thought-provoking visions of life and death', 'Quirky and endearing relationships', 'Amusing jokes and witty satire', 'Laugh-out-loud relationship entanglements', 'Show All…'], ['Crude humor and satire', 'Humanity and the world around us', 'Dreamlike,  quirky,  and surreal storytelling', 'Gags,  jokes,  and slapstick humor', 'Amusing jokes and witty satire', 'Funny jokes and crude humor', 'Show All…'], ['Epic heroes', 'Superheroes in action-packed battles with villains', 'Emotional and captivating fantasy storytelling', 'Action comedy and silly heroics', 'Adrenaline-fueled action and fast cars', 'Show All…'], ['Moving relationship stories', 'Captivating relationships and charming romance', 'Charming romances and delightful chemistry', 'Passion and romance', 'Touching and sentimental family stories', 'Show All…'], ['Humanity and the world around us', 'Surreal and thought-provoking visions of life and death', 'Dreamlike,  quirky,  and surreal storytelling', 'Show All…'], ['Horror, the undead and monster classics', 'Intense violence and sexual transgression', 'Terrifying,  haunted,  and supernatural horror', 'Creepy,  chilling,  and terrifying horror', 'Twisted dark psychological thriller', 'Gothic and eerie haunting horror', 'Gory,  gruesome,  and slasher horror', 'Show All…'], ['Moving relationship stories', 'Thrillers and murder mysteries', 'Humanity and the world around us', 'Suspenseful crime thrillers', 'Enduring stories of family and marital drama', 'Gripping,  intense violent crime', 'Heartbreaking and moving family drama', 'Intriguing and suspenseful murder mysteries', 'Show All…'], ['Thrillers and murder mysteries', 'Crime, drugs and gangsters', 'Gripping,  intense violent crime', 'Suspenseful crime thrillers', 'Racism and the powerful fight for justice', 'Intense political and terrorist thrillers', 'Violent crime and drugs', 'Show All…'], ['Moving relationship stories', 'Intense violence and sexual transgression', 'Humanity and the world around us', 'Erotic relationships and desire', 'Challenging or sexual themes & twists', 'Enduring stories of family and marital drama', 'Powerful stories of heartbreak and suffering', 'Emotional teen coming-of-age stories', 'Show All…'], ['Humanity and the world around us', 'Nazis and World War II', 'Surreal and thought-provoking visions of life and death', 'Graphic violence and brutal revenge', 'Gripping,  intense violent crime', 'Terrifying,  haunted,  and supernatural horror', 'Show All…'], ['Humanity and the world around us', 'Emotional teen coming-of-age stories', 'Emotional LGBTQ relationships', 'Powerful poetic and passionate drama', 'Show All…']]
+
+Example output form above print statements
+
+***New update, also returns a dictionary that maps each movie to a list of genres and list of themes.
+'''
