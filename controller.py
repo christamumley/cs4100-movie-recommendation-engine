af38a0fb1a44f3cdb9b8497a2767d0b49c4e8016
Christa Mumley <mumley.c@husky.neu.edu>
2024-04-09 18:27:44 -0400

    loaded default responses for questions that do not need any model retrieval

--- controller.py
+++ controller.py
@@ -1,9 +1,10 @@
 import os
 import sys
 import threading
+import numpy as np
 
 # chatbot implementatipon
-from chatbot.moviebot import get_label
+from chatbot.moviebot import get_label, get_default_responses
 
 # movie model implmentation
 from Models.movie_models import (
@@ -11,11 +12,6 @@ from Models.movie_models import (
     recommend_movies_based_on_genre,
     recommend_movies_based_on_similarity,
 )
-
-##########################################
-#    NEEDS to be implemented
-##########################################
-
 
 def handle_msg(msg):
     label = get_label(msg)
@@ -26,4 +22,8 @@ def handle_msg(msg):
     elif label == "similar":
         return recommend_movies_based_on_similarity(msg).to_string(index=False)
     else:
-        return "Sorry, I'm not sure I understand what you're looking for. Could you reword that?"
+        responses = get_default_responses() 
+        if(label in responses.keys()):
+            return np.random.choice(responses[label])
+        else: 
+            return "Sorry, I'm not sure I understand what you're looking for. Could you reword that?"
